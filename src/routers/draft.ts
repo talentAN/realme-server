d25dbcc858024013fc8ce9ebe1979d6c0bf5083e
AzAz <az15921869329@gmail.com>
2020-05-12 13:29:09 +0800

    fix draft list sql

--- src/routers/draft.ts
+++ src/routers/draft.ts
@@ -6,15 +6,18 @@ import {
   publishDraft,
   deleteDraft,
   countMyDrafts,
+  queryList,
+  queryItem,
 } from '../sql/draft';
 
 enum Type {
   Add = 'add',
   Update = 'update',
   Delete = 'delete',
-  Query = 'query',
   Publish = 'publish',
   Count = 'count',
+  QueryList = 'query_ist',
+  QueryItem = 'query_item',
 }
 export default function draft(router) {
   router.post('/draft', async (ctx, next) => {
@@ -33,6 +36,10 @@ export default function draft(router) {
         result = await queryDB({id, title, content}, ctx, updateDraft);
         result = Array.isArray(result) ? {status: 'success'} : {status: 'fail'};
         break;
+      case Type.Delete:
+        result = await queryDB(id, ctx, deleteDraft);
+        result = Array.isArray(result) ? {status: 'success', id} : {status: 'fail'};
+        break;
       case Type.Publish:
         Logger(`Publish draft: ${id}`);
         //查询
@@ -49,6 +56,15 @@ export default function draft(router) {
         result = Array.isArray(result)
           ? {status: 'success', count: result[0].count}
           : {status: 'fail'};
+        break;
+      case Type.QueryList:
+        const {limit, offset} = ctx.request.body;
+        result = await queryDB({limit, offset, user}, ctx, queryList);
+        result = Array.isArray(result) ? {status: 'success', data: result} : {status: 'fail'};
+        break;
+      case Type.QueryItem:
+        result = await queryDB(id, ctx, queryItem);
+        result = Array.isArray(result) ? {status: 'success', data: result[0]} : {status: 'fail'};
         break;
       default:
         result = false;
--- src/sql/draft.ts
+++ src/sql/draft.ts
@@ -6,15 +6,13 @@ export const addDraft = ({
 }) => `
 INSERT INTO "draft" ("id","type","title", "content", "auth", "last_modified") VALUES('${id}', '${type}', '', '', '${user}', '${last_modified}')
 `;
-
 export const updateDraft = ({id, title, content}) =>
   `update "draft" set title='${title}', content='${content}' where id='${id}'`;
-
 export const queryDraft = (id: string) => `select * from "draft" where id='${id}'`;
-
 export const publishDraft = ({id, title, content, type, auth, last_modified}) =>
   `INSERT INTO "chapter" ("id", "title", "content", "type", "auth", "publish_date") VALUES('${id}', '${title}', '${content}', '${type}', '${auth}', '${last_modified}')`;
-
 export const deleteDraft = (id: string) => `DELETE FROM "draft" where id='${id}'`;
-
 export const countMyDrafts = (id: string) => `select count(*) from "draft" where "auth"='${id}'`;
+export const queryList = ({offset, limit, user}) =>
+  `select * from "draft" where "auth"='${user}' ORDER by last_modified DESC OFFSET ${offset} LIMIT ${limit};`;
+export const queryItem = (id: string) => `select * from "draft" where "id"='${id}'`;
