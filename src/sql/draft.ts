ea115c6b12f68529dfb4feb16a15dd70f768d3b7
zhen.an <az15921869329@gmail.com>
2020-05-17 13:59:49 +0800

    draft done

--- src/sql/draft.ts
+++ src/sql/draft.ts
@@ -12,7 +12,8 @@ export const queryDraft = (id: string) => `select * from "draft" where id='${id}
 export const publishDraft = ({id, title, content, type, auth, last_modified}) =>
   `INSERT INTO "chapter" ("id", "title", "content", "type", "auth", "publish_date") VALUES('${id}', '${title}', '${content}', '${type}', '${auth}', '${last_modified}')`;
 export const deleteDraft = (id: string) => `DELETE FROM "draft" where id='${id}'`;
-export const countMyDrafts = (id: string) => `select count(*) from "draft" where "auth"='${id}'`;
+export const countMyDrafts = (id: string) =>
+  `select count(*) from "draft" where "auth"='${id}'and ("title" <> '' or "content" <> '')`;
 export const queryList = ({offset, limit, user}) =>
-  `select * from "draft" where "auth"='${user}' ORDER by last_modified DESC OFFSET ${offset} LIMIT ${limit};`;
+  `select * from "draft" where "auth"='${user}' and ("title" <> '' or "content" <> '') ORDER by last_modified DESC OFFSET ${offset} LIMIT ${limit};`;
 export const queryItem = (id: string) => `select * from "draft" where "id"='${id}'`;
